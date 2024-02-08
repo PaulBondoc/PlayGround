ad7123e9c82f39e7555fec5434fcac19d0791296
Paul Bondoc <pjbndc@gmail.co>
2024-02-08 11:33:55 +0800

    remove constant folder from gitignore

---
+++ src/constant/CategoriesData.jsx
@@ -0,0 +1,34 @@
+export const categoryList = [
+  {
+    id: null,
+    name: "All",
+  },
+  {
+    id: 1,
+    name: "Templates",
+  },
+  {
+    id: 2,
+    name: "Frontend Tools",
+  },
+  {
+    id: 3,
+    name: "Colors",
+  },
+  {
+    id: 4,
+    name: "Icons",
+  },
+  {
+    id: 5,
+    name: "Typography",
+  },
+  {
+    id: 6,
+    name: "JS Libraries",
+  },
+  {
+    id: 7,
+    name: "Favorites",
+  },
+];
---
+++ src/constant/ResourcesData.jsx
@@ -0,0 +1,272 @@
+import Uiverse from "../assets/logos/uiverse.png";
+import Omatsuri from "../assets/logos/omatsuri.png";
+import CssPortal from "../assets/logos/cssportal.png";
+import Uicolors from "../assets/logos/uicolors.png";
+
+import L0015 from "../assets/logos/10015.png";
+import Hoverdev from "../assets/logos/hoverdev.svg";
+import Landingfolio from "../assets/logos/landingfolio.svg";
+import Lordicon from "../assets/logos/lordicon.png";
+
+import Dribbble from "../assets/logos/dribbble.png";
+import RealtimeColors from "../assets/logos/realtimecolors.png";
+import DaisyUI from "../assets/logos/daisyui.svg";
+import Undraw from "../assets/logos/undraw.png";
+
+import Bgjar from "../assets/logos/bgjar.png";
+import Atropos from "../assets/logos/atroposjs.png";
+import CSSButtons from "../assets/logos/cssbuttons.png";
+import FeatherIcons from "../assets/logos/feathericons.png";
+
+import Ionicons from "../assets/logos/ionicons.png";
+import Fontawesome from "../assets/logos/fontawesome.png";
+import Transition from "../assets/logos/transition.png";
+import Getwaves from "../assets/logos/getwaves.svg";
+
+import Blobmaker from "../assets/logos/blobmaker.png";
+import Googlefonts from "../assets/logos/googlefonts.png";
+import ScrollReveal from "../assets/logos/scrollreveal.png";
+import Webflow from "../assets/logos/webflow.png";
+
+export const resourcesList = [
+  {
+    name: "UIVerse",
+    id: 2,
+    url: "https://uiverse.io/",
+    description:
+      "Library Of Free And Customizable UI Elements Made With CSS Or Tailwind.",
+    logo: Uiverse,
+  },
+  {
+    name: "Omatsuri",
+    id: 2,
+    url: "https://omatsuri.app/",
+    description:
+      "Omatsuri is a progressive web application with 12 open source frontend focused tools.",
+    logo: Omatsuri,
+  },
+  {
+    name: "CSS Portal",
+    id: 2,
+    url: "https://www.cssportal.com/",
+    description:
+      "Simplifying web design with tools, generators, and resources for CSS and HTML projects.",
+    logo: CssPortal,
+  },
+  {
+    name: "UIColors",
+    id: 3,
+    url: "https://uicolors.app/",
+    description: "Tailwind CSS color generator.",
+    logo: Uicolors,
+  },
+  {
+    name: "10015",
+    id: 2,
+    url: "https://10015.io/",
+    description: "Generate everything using css (e.g. loaders, buttons).",
+    logo: L0015,
+  },
+  {
+    name: "hover.dev",
+    id: 2,
+    url: "https://www.hover.dev/",
+    description: "Animated UI Components for React & Tailwind.",
+    logo: Hoverdev,
+  },
+  {
+    name: "Landingfolio",
+    id: 1,
+    url: "https://www.landingfolio.com/",
+    description:
+      "The Best Landing Page Design Inspiration, Templates and More.",
+    logo: Landingfolio,
+  },
+  {
+    name: "LordIcon",
+    id: 4,
+    url: "https://lordicon.com/",
+    description:
+      "Library of carefully crafted animated icons, ready to use in digital products, presentations, or videos!",
+    logo: Lordicon,
+  },
+  {
+    name: "Curated.design",
+    id: 1,
+    url: "https://www.curated.design/",
+    description:
+      "Unleash your creativity with out-of-this-world web design inspiration from across the web.",
+    logo: "",
+  },
+  {
+    name: "Animate.css",
+    id: 2,
+    url: "https://animate.style/",
+    description:
+      "Easily add stunning CSS animations to your web projects for enhanced visual appeal.",
+    logo: "",
+  },
+  {
+    name: "Dribbble",
+    id: 1,
+    url: "https://dribbble.com/",
+    description:
+      "Get inspired by the work of millions of top-rated designers & agencies around the world.",
+    logo: Dribbble,
+  },
+  {
+    name: "Realtime Colors",
+    id: 3,
+    url: "https://www.realtimecolors.com/",
+    description: "Visualize Your Colors & Fonts On a Real Site.",
+    logo: RealtimeColors,
+  },
+  {
+    name: "DaisyUI",
+    id: 2,
+    url: "https://daisyui.com/",
+    description: "The most popular component library for Tailwind CSS.",
+    logo: DaisyUI,
+  },
+  {
+    name: "unDRAW.co",
+    id: 2,
+    url: "https://undraw.co/",
+    description:
+      "Browse to find the images that fit your needs and click to download.",
+    logo: Undraw,
+  },
+  {
+    name: "BGJar",
+    id: 2,
+    url: "https://bgjar.com/",
+    description:
+      "Free svg background generator for your websites, blogs and app.",
+    logo: Bgjar,
+  },
+  {
+    name: "ShapeDividers",
+    id: 2,
+    url: "https://shapedividers.com/",
+    description:
+      "The easiest way to add vertical, responsive or animated Shape Dividers to your websites.",
+    logo: "",
+  },
+  {
+    name: "Data AOS",
+    id: 6,
+    url: "https://michalsnik.github.io/aos/",
+    description:
+      "A JavaScript library for effortless 'animate on scroll' effects, elevating web page interactivity.",
+    logo: "",
+  },
+  {
+    name: "AtroposJS",
+    id: 6,
+    url: "https://atroposjs.com/",
+    description:
+      "Craft touch-friendly 3D parallax hover effects effortlessly with this free and open-source library.",
+    logo: Atropos,
+  },
+  {
+    name: "ParticlesJS",
+    id: 6,
+    url: "https://particles.js.org/",
+    description:
+      "library that lets you create and customize particles, confetti and fireworks animations for your website.",
+    logo: "",
+  },
+  {
+    name: "CSS Buttons",
+    id: 6,
+    url: "https://cssbuttons.app/",
+    description:
+      "library that lets you create and customize particles, confetti and fireworks animations for your website.",
+    logo: CSSButtons,
+  },
+  {
+    name: "Feather Icons",
+    id: 4,
+    url: "https://feathericons.com/",
+    description: "Simply beautiful open source icons.",
+    logo: FeatherIcons,
+  },
+  {
+    name: "Ionicons",
+    id: 4,
+    url: "https://ionic.io/ionicons",
+    description: "Open source icons. Lovingly hand-crafted.",
+    logo: Ionicons,
+  },
+  {
+    name: "Fontawesome",
+    id: 4,
+    url: "https://fontawesome.com/",
+    description:
+      "Font Awesome is the Internet's icon library and toolkit, used by millions of users.",
+    logo: Fontawesome,
+  },
+  {
+    name: "transition.style",
+    id: 2,
+    url: "https://www.transition.style/",
+    description: "Drop-in CSS transitions",
+    logo: Transition,
+  },
+  {
+    name: "Getwaves",
+    id: 2,
+    url: "https://getwaves.io/",
+    description:
+      "a tool that lets you create unique and customizable SVG waves for your next web design.",
+    logo: Getwaves,
+  },
+  {
+    name: "Blobmaker",
+    id: 2,
+    url: "https://www.blobmaker.app/",
+    description:
+      "a web app that lets you create random, unique, and organic-looking SVG shapes based on random data.",
+    logo: Blobmaker,
+  },
+  {
+    name: "Clippy",
+    id: 2,
+    url: "https://www.blobmaker.app/",
+    description:
+      "Clippy is a tool that lets you create and customize CSS clip-path shapes for elements. ",
+    logo: "",
+  },
+  {
+    name: "Google Fonts",
+    id: 5,
+    url: "https://fonts.google.com/",
+    description:
+      "Google Fonts makes it easy to bring personality and performance to your websites and products.",
+    logo: Googlefonts,
+  },
+  {
+    name: "AnimeJS",
+    id: 6,
+    url: "https://animejs.com/",
+    description:
+      "a JavaScript library that lets you create and control animations for HTML, CSS, SVG and other web elements.",
+    logo: "",
+  },
+  {
+    name: "ScrollReveal",
+    id: 6,
+    url: "https://scrollrevealjs.org/",
+    description:
+      "A powerful new version of the web's favorite scroll animation library, and brand new documentation.",
+    logo: ScrollReveal,
+  },
+  {
+    name: "Webflow",
+    id: 1,
+    url: "https://webflow.com/",
+    description:
+      "Experience the power of HTML, CSS, and JavaScript in a 100% visual canvas.",
+    logo: Webflow,
+  },
+];
